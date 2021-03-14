d9c8cd3940a7492d2dc413d9091d1bda355c88c4
yongfangzhang <zhangyongfang_2021@126.com>
2021-03-14 18:27:58 +0800

    fix login

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -191,7 +191,7 @@ export default {
             .dispatch('user/login', this.loginForm)
             .then(() => {
               this.$router.push({
-                path: this.redirect || '/',
+                path: this.redirect || '/dashboard/index',
                 query: this.otherQuery
               });
               this.loading = false;
