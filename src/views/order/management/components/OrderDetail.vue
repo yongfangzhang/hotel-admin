6b05774a844ce09edb92b904b6fee5b9a704336e
yongfangzhang <zhangyongfang_2021@126.com>
2021-06-12 12:19:50 +0800

    optimize

--- src/views/order/management/components/OrderDetail.vue
+++ src/views/order/management/components/OrderDetail.vue
@@ -1,29 +1,69 @@
 <template>
-  <div v-if="currentOrder">
-    <div v-for="item in currentOrder.items"
-         :key="item.uuid">
-      <div class="pb-3 border-bottom font-weight-bold">入住人: {{ item.name }} ({{ item.mobile }})</div>
+  <div v-if="currentOrder" class="m-view-panel-sm">
+    <div class="pb-3 border-bottom">
+      <div class="pb-3 border-bottom font-weight-bold">订单: {{ currentOrder.number }}</div>
       <div class="m-view-item">
-        <span class="label">公寓</span>
-        <m-view type="selector"
+        <span class="label">渠道</span>
+        <m-view class="order-detail-view"
+                :value="currentOrder.channelName" />
+      </div>
+      <div class="m-view-item">
+        <span class="label">房价</span>
+        <m-view type="currency"
                 class="order-detail-view"
-                :map="apartmentMap"
-                :value="item.apartmentUuid" />
+                :value="currentOrder.paidPrice" />
       </div>
       <div class="m-view-item">
-        <span class="label">房间</span>
-        <m-view class="order-detail-view"
-                :value="item.room.name" />
+        <span class="label">商品</span>
+        <m-view type="currency"
+                class="order-detail-view"
+                :value="currentOrder.productIncome" />
       </div>
       <div class="m-view-item">
-        <span class="label">入住时间</span>
-        <m-view class="order-detail-view"
-                :value="item.liveAt" />
+        <span class="label">押金</span>
+        <m-view type="currency"
+                class="order-detail-view"
+                :value="currentOrder.deposit" />
       </div>
       <div class="m-view-item">
-        <span class="label">退房时间</span>
-        <m-view class="order-detail-view"
-                :value="item.leaveAt" />
+        <span class="label">押金扣除</span>
+        <m-view type="currency"
+                class="order-detail-view"
+                :value="currentOrder.depositDeduction" />
+      </div>
+      <div class="m-view-item">
+        <span class="label">收益</span>
+        <m-view type="currency"
+                class="order-detail-view"
+                :value="currentOrder.paidPrice + currentOrder.productIncome" />
+      </div>
+    </div>
+    <div class="pt-3">
+      <div v-for="item in currentOrder.items"
+           :key="item.uuid">
+        <div class="pb-3 border-bottom font-weight-bold">入住人: {{ item.name }} ({{ item.mobile }})</div>
+        <div class="m-view-item">
+          <span class="label">公寓</span>
+          <m-view type="selector"
+                  class="order-detail-view"
+                  :map="apartmentMap"
+                  :value="item.apartmentUuid" />
+        </div>
+        <div class="m-view-item">
+          <span class="label">房间</span>
+          <m-view class="order-detail-view"
+                  :value="item.room.name" />
+        </div>
+        <div class="m-view-item">
+          <span class="label">入住时间</span>
+          <m-view class="order-detail-view"
+                  :value="item.liveAt" />
+        </div>
+        <div class="m-view-item">
+          <span class="label">退房时间</span>
+          <m-view class="order-detail-view"
+                  :value="item.leaveAt" />
+        </div>
       </div>
     </div>
   </div>
--- src/views/statistics/history.vue
+++ src/views/statistics/history.vue
@@ -1,66 +1,78 @@
 <template>
-  <div class="h-100 d-flex flex-column">
-    <div class="d-flex justify-content-between">
-      <el-form class="pt-3 px-3"
-               label-position="left"
-               inline
-               @submit.native.prevent>
-        <div>
-          <datetime-filter v-model="createdRange"
-                           label="接单日期(最大区间7天)"
-                           :width="dateFilterWidth" />
-        </div>
-      </el-form>
+  <div class="h-100">
+    <div class="h-100 d-flex flex-column">
+      <div class="d-flex justify-content-between">
+        <el-form class="pt-3 px-3"
+                 label-position="left"
+                 inline
+                 @submit.native.prevent>
+          <div>
+            <datetime-filter v-model="createdRange"
+                             label="接单日期(最大区间7天)"
+                             :width="dateFilterWidth" />
+          </div>
+        </el-form>
+      </div>
+      <div class="d-flex px-3 pb-3 flex-fill flex-column scrollable-y">
+        <el-table :data="roomList"
+                  border
+                  stripe
+                  class="w-100 h-100 flex-fill scrollable-y">
+          <el-table-column label="公寓"
+                           align="center"
+                           prop="apartmentUuid"
+                           :width="colWidth.sm">
+            <template slot-scope="{ row }">
+              <m-view :value="apartmentMap[row.apartmentUuid]" />
+            </template>
+          </el-table-column>
+          <el-table-column label="房间号"
+                           align="center"
+                           prop="number"
+                           :width="colWidth.sm" />
+          <el-table-column label="房间类型"
+                           align="center"
+                           prop="typeUuid"
+                           :width="colWidth.sm">
+            <template slot-scope="{ row }">
+              <m-view :value="ROOM_TYPE_MAP[row.typeUuid]" />
+            </template>
+          </el-table-column>
+          <el-table-column v-for="col in dateColumns"
+                           :key="col"
+                           :label="col"
+                           header-align="center"
+                           :min-width="colWidth.date">
+            <template slot-scope="{ row, column}">
+              <div v-if="roomOrderMap[column.label] && roomOrderMap[column.label][row.uuid]">
+                <el-button v-for="item in roomOrderMap[column.label][row.uuid]"
+                           :key="item.uuid"
+                           type="text"
+                           class="m-0 text-left"
+                           @click="showOrderDetail(item.order)">
+                  <div>姓名: {{ item.name }}</div>
+                  <div>渠道: {{ item.order.channelName }}</div>
+                  <div>房价: {{ item.order.paidPrice }}</div>
+                  <div>商品: {{ item.order.productIncome }}</div>
+                  <div>收益: {{ item.order.paidPrice + item.order.productIncome }}</div>
+                </el-button>
+              </div>
+              <div v-else
+                   class="text-center">--</div>
+            </template>
+          </el-table-column>
+        </el-table>
+      </div>
     </div>
-    <div class="d-flex px-3 pb-3 flex-fill flex-column scrollable-y">
-      <el-table :data="roomList"
-                border
-                stripe
-                class="w-100 h-100 flex-fill scrollable-y">
-        <el-table-column label="公寓"
-                         align="center"
-                         prop="apartmentUuid"
-                         :width="colWidth.sm">
-          <template slot-scope="{ row }">
-            <m-view :value="apartmentMap[row.apartmentUuid]" />
-          </template>
-        </el-table-column>
-        <el-table-column label="房间号"
-                         align="center"
-                         prop="number"
-                         :width="colWidth.sm" />
-        <el-table-column label="房间类型"
-                         align="center"
-                         prop="typeUuid"
-                         :width="colWidth.sm">
-          <template slot-scope="{ row }">
-            <m-view :value="ROOM_TYPE_MAP[row.typeUuid]" />
-          </template>
-        </el-table-column>
-        <el-table-column v-for="col in dateColumns"
-                         :key="col"
-                         :label="col"
-                         header-align="center"
-                         :min-width="colWidth.date">
-          <template slot-scope="{ row, column}">
-            <div v-if="roomOrderMap[column.label] && roomOrderMap[column.label][row.uuid]">
-              <el-button v-for="item in roomOrderMap[column.label][row.uuid]"
-                         :key="item.uuid"
-                         type="text"
-                         class="m-0 text-left">
-                <div>姓名: {{ item.name }}</div>
-                <div>渠道: {{ item.order.channelName }}</div>
-                <div>房价: {{ item.order.paidPrice }}</div>
-                <div>商品: {{ item.order.productIncome }}</div>
-                <div>收益: {{ item.order.paidPrice + item.order.productIncome }}</div>
-              </el-button>
-            </div>
-            <div v-else
-                 class="text-center">--</div>
-          </template>
-        </el-table-column>
-      </el-table>
-    </div>
+    <m-dialog v-model="showDetailDialog"
+              title="订单详情"
+              :has-close="true"
+              :has-confirm="false"
+              :append-to-body="true"
+              width="sm">
+      <order-detail :current-order="currentOrder"
+                    :apartment-map="apartmentMap" />
+    </m-dialog>
   </div>
 </template>
 <script>
@@ -69,17 +81,22 @@ import { PARSE_TIME_TYPE } from '@/utils/constant';
 import { ACTIONS, MODULE } from '@/store/constant';
 import { baseTableMixin } from '@/utils/mixins';
 import { mapGetters } from 'vuex';
+import OrderDetail from '@/views/order/management/components/OrderDetail.vue';
+
 const MAX_DAYS = 6;
 const RANGE_MILL_SECONDS = MAX_DAYS * 24 * 3600 * 1000;
 
 export default {
   name: 'StatisticsHistory',
+  components: { OrderDetail },
   mixins: [baseTableMixin],
   data() {
     const begin = new Date();
     begin.setDate(begin.getDate() - MAX_DAYS);
     const end = new Date();
     return {
+      currentOrder: null,
+      showDetailDialog: false,
       queries: {
         createdAtStart: null,
         createdAtStop: null
@@ -163,6 +180,10 @@ export default {
         console.log(map);
         this.roomOrderMap = map;
       });
+    },
+    showOrderDetail(order) {
+      this.currentOrder = order;
+      this.showDetailDialog = true;
     }
   }
 };
